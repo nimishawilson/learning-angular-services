665624e89cd9cbe338473fc05db2711ceeee65e8
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-08 16:56:03 +0530

    injecting services into serevices

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,7 @@
+import { LoggingService } from "./logging.service";
+import { Injectable } from "@angular/core";
+
+@Injectable()
 export class AccountsService{
      accounts = [
     {
@@ -14,8 +18,11 @@ export class AccountsService{
     }
   ];
 
+  constructor(private loggingService: LoggingService){ }
+
   addAccount(name: string, status: string){
       this.accounts.push({name: name, status: status});
+      this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string){
